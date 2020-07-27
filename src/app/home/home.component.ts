62d2909ee54224a359a63e8c253bff443c910b31
hoj <joh.l.austin@gmail.com>
2020-07-27 15:55:58 +1000

    polaroids

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,24 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+    var slideIndex = 0;
+    carousel();
+
+
+    function carousel() {
+      var i: any
+      let eles= document.getElementsByClassName("slides") as HTMLCollectionOf<HTMLElement>
+      for (i = 0; i < eles.length; i++) {
+        eles[i].style.display = "none"; 
+      }
+      slideIndex++;
+      
+      if (slideIndex > eles.length) {slideIndex = 1} 
+      eles[slideIndex-1].style.display = "block"; 
+      setTimeout(carousel, 5000); 
+    }
+  }
 }
+
+
